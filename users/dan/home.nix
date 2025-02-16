7b7a5ab2f153b931b3a4020ac135be39b20b10dd
nocivic <nocivic@proton.me>
2025-02-16 19:45:02 -0300

    remove mpd launcher script and sources configuration

--- users/dan/home.nix
+++ users/dan/home.nix
@@ -128,34 +128,6 @@
   # Install firefox.
   programs.firefox.enable = true;
 
-  # Home Manager managing dotfiles.
-  home.file.".config/mpd-sources/sources.txt".text = ''
-    YouTube https://www.youtube.com/watch?v=dQw4w9WgXcQ
-    Lofi https://www.youtube.com/watch?v=jfKfPfyJRdk
-    Jazz https://www.youtube.com/watch?v=Dx5qFachd3A
-    Synthwave https://www.youtube.com/watch?v=vc3s6wJklb0
-  '';
-
-  home.file.".local/bin/mpd-launcher".text = ''
-  #!/bin/sh
-  MENU="wofi --dmenu"
-  [ -n "$XDG_SESSION_TYPE" ] && [ "$XDG_SESSION_TYPE" = "x11" ] && MENU="dmenu"
-
-  choice=$(cut -d ' ' -f1 ~/.config/mpd-sources/sources.txt | $MENU)
-  [ -z "$choice" ] && exit 1
-
-  url=$(grep "^$choice " ~/.config/mpd-sources/sources.txt | cut -d ' ' -f2-)
-  [ -z "$url" ] && exit 1
-
-  audio_url=$(yt-dlp -f 'bestaudio' -g "$url")
-  mpc clear
-  mpc add "$audio_url"
-  mpc play
-  '';
-
-  home.file.".local/bin/mpd-launcher".executable = true;
-
-
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
