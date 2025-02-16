852ab7f0a65e53966fb0f8a2333915688fc27584
nocivic <nocivic@proton.me>
2025-02-16 15:30:24 -0300

    hyprland confs

--- users/dan/home.nix
+++ users/dan/home.nix
@@ -22,14 +22,30 @@
     # etc...
   ];
   
-  # Hyprland config
-  programs.hyprland = {
+  # Hyprland Confs
+  wayland.windowManager.hyprland = {
     enable = true;
-    extraConfig = ''
-      bind = SUPER, M, exec, mpd-launcher
-    '';
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, F, exec, firefox"
+        "$mod, M, exec, mpd-launcher"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+            let ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9)
+      );
+    };
   };
-  
+
   # Add git config
   programs.git = {
     enable = true;
